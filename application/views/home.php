763b3acf72b0cf6f9e5cced10a5fcb6fcbc81231
hanasoke@gmail.com <hanasoke@gmail.com>
2025-03-09 23:38:32 +0700

    adding new files of cars data

--- application/views/home.php
+++ application/views/home.php
@@ -12,17 +12,17 @@
 		/>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<nav class="navbar navbar-expand-lg bg-secondary">
 			<div class="container">
 				<a class="navbar-brand" href="#">E-COMMERCE</a>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">Home</a>
+							<a class="nav-link active text-primary fw-semibold" aria-current="page" href="<?php print site_url('sellers') ?>">Sellers</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">Link</a>
+							<a class="nav-link text-white" href="#">Cars</a>
 						</li>
 					</ul>
 				</div>
